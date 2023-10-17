1a4b7b51a94e31b709ea129ba50f469391b0704c
shingaryu <shingaryu@gmail.com>
2023-10-18 01:36:47 +0900

    自分のチャンネルの動画のみを取得するためにmineオプションを使用するように改良、トークンの更新チェックを挿入

--- repositories/youtube_repository.py
+++ repositories/youtube_repository.py
@@ -1,61 +1,54 @@
 from __future__ import print_function
 
 import os.path
+from dotenv import load_dotenv
+load_dotenv()
 
+from common.get_logger import get_logger
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+logger = get_logger(__name__, os.environ.get("LOGGER_LEVEL"))
+
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+api_service_name = "youtube"
+api_version = "v3"
 
 if os.path.exists('token.json'):
     creds = Credentials.from_authorized_user_file('token.json', scopes)
 else:
     raise Exception("token.jsonがありません。")
 
-if creds.expired and creds.refresh_token:
+
+def refresh_token_if_expired():
+    if not creds.expired:
+        return
+    if not creds.refresh_token:
+        return
+    logger.debug("token expired. refreshing...")
     creds.refresh(Request())
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
+    logger.debug("token refresh completed")
 
-api_service_name = "youtube"
-api_version = "v3"
 
+refresh_token_if_expired()
 youtube = build(api_service_name, api_version, credentials=creds)
 
-my_channel_id = None
-
-def get_my_channel():
-    try:
-        request = youtube.channels().list(
-            part="snippet",
-            mine=True
-        )
-
-        response = request.execute()
-
-        return response.get("items")[0]
-    except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f'An error occurred: {error}')
-
-def init_my_channel_id():
-    global my_channel_id
-    if my_channel_id is None:
-        my_channel_id = get_my_channel().get("id")
 
 def get_my_recent_videos():
-    init_my_channel_id()
-
+    logger.info("get my recent videos...")
+    refresh_token_if_expired()
     request = youtube.search().list(
         part="snippet",
         maxResults=50,  # YouTube APIの単一のリクエストで取得できる最大数
-        channelId=my_channel_id,
+        forMine=True,
         type="video",
         order="date"
     )
 
     response = request.execute()
-    # print(response)
+    logger.debug(response)
     return response.get("items")
